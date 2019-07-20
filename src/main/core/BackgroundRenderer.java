14db533cea1f9317e3655589cdc2c9001a58dc11
KJAstudios <kja@surewest.net>
2019-07-19 23:44:30 -0700

    created and started the renderer for game backgrounds

---
+++ src/main/core/BackgroundRenderer.java
@@ -0,0 +1,17 @@
+package core;
+
+//This class handles all loading/unloading of static background elements for the game
+public class BackgroundRenderer {
+
+    private int renderOriginX; // (x, y) coordinates of where we want the image to be loaded at
+    private int renderOriginY;
+
+    public BackgroundRenderer(String imageToLoad, int xCoord, int yCoord) { // TODO add image file variable
+	renderOriginX = xCoord; // set the class variable to the input varables
+	renderOriginY = yCoord;
+    }
+
+    private void loadImage(String filename) { // TODO create image loader
+
+    }
+}
