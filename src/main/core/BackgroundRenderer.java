13e65d80a75b9123edafeaff05c4cb0c42981d59
KJAstudios <kja@surewest.net>
2019-07-20 19:52:33 -0700

    Created basic background renderer

--- src/main/core/BackgroundRenderer.java
+++ src/main/core/BackgroundRenderer.java
@@ -1,18 +1,38 @@
 package core;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 //This class handles all loading/unloading of static background elements for the game
 public class BackgroundRenderer {
 
-    private int renderOriginX; // (x, y) coordinates of where we want the image to be loaded at
-    private int renderOriginY;
+    // private int renderOriginX; // (x, y) coordinates of where we want the image
+    // to be loaded at
+    // private int renderOriginY;
+    // TODO figure out how to make a coordanate system for drawing images
 
-    public BackgroundRenderer(String imageToLoad, int xCoord, int yCoord) { // TODO add image file variable
-	renderOriginX = xCoord; // set the class variable to the input varables
-	renderOriginY = yCoord;
-	loadImage(imageToLoad);
+    public BackgroundRenderer(String imageToLoad, /* int xCoord, int yCoord, */ JFrame loadFrame) { // TODO add image
+												    // file variable
+	// renderOriginX = xCoord; // retrieve the coordantates that we want the image
+	// loaded at
+	// renderOriginY = yCoord;
+	JLabel imageLabel = new JLabel(new ImageIcon(loadImage(imageToLoad)));
+	loadFrame.add(imageLabel);
     }
 
-    private void loadImage(String filename) { // TODO create image loader
-
+    private BufferedImage loadImage(String filename) { // this loads the image to display
+	BufferedImage backgroundImg = null;
+	try {
+	    backgroundImg = ImageIO.read(new File("assets/" + filename));
+	    return backgroundImg;
+	} catch (IOException e) {
+	    return null;
+	}
     }
 }
--- src/main/core/Main.java
+++ src/main/core/Main.java
@@ -4,14 +4,15 @@ import javax.swing.JFrame;
 
 public class Main {
 
-	public Main() {
-		throw new UnsupportedOperationException("Constructor for Main not yet implemented.");
-	}
+    public Main() {
+	throw new UnsupportedOperationException("Constructor for Main not yet implemented.");
+    }
 
-	public static void main(String[] args) {
-		var window = new JFrame("Hello World");
-		window.setSize(500, 500);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setVisible(true);
-	}
+    public static void main(String[] args) {
+	var window = new JFrame("Hello World");
+	BackgroundRenderer TestBackground = new BackgroundRenderer("Grass 4x4.png", window);// load background image
+	window.setSize(500, 500);
+	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	window.setVisible(true);
+    }
 }
