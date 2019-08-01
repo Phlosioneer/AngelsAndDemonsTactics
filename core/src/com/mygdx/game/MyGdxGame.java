962f314e4cde454146e435a3fb326abe85f8cbdd
KJAstudios <kja@surewest.net>
2019-07-31 21:12:02 -0700

    now renders our background

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,29 +5,42 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	SpriteBatch batch; //class used to draw 2D images
+	Texture backgroundImg; //create a texture object (stored in VRAM)
+	private OrthographicCamera mainCamera; //creates the main camera object
+	private java.awt.Rectangle background;
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+	public void create () { //runs once on game startup
+		mainCamera = new OrthographicCamera();
+		mainCamera.setToOrtho(false, 1920, 1080); //sets camera to always show a 1920x1080 view
+		//TODO decide on game resolution
+		batch = new SpriteBatch(); //instantize the SpriteBatch
+		backgroundImg = new Texture("Grass 4x4.png"); //load background texture
+		background = new Rectangle(); //this will determine the position of the background
+		background.x = 0; //set the x/y coords of the background
+		background.y = 0;
+		}
+		
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
+	public void render () { //frame update, aka main game loop
+		Gdx.gl.glClearColor(0, 0, 0, 1); //sets the 'clear' color for openGL (red, green, blue, alpha)
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);//clears the screen, my understanding is it sets it to the 'clear' color
+		mainCamera.update();//updates the camera every frame
+		batch.setProjectionMatrix(backgroundCamera.combined);//tells the batch system to use the camera's coord system
+		batch.begin(); //used to please openGL. everything between batch.begin and batch.end will render
+		//once the batch.end command is sent
+		batch.draw(backgroundImg, background.x, background.y);
 		batch.end();
 	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose () { //cleanup for shutting down the game
+		batch.dispose(); //clean the loaded files from memory to help Java's garbage collection
+		backgroundImg.dispose();
 	}
 }
