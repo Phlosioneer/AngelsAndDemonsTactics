5dbb5d23c0a5ca16e5099763965cb1ac29587067
KJAstudios <kja@surewest.net>
2019-08-01 23:25:45 -0700

    changed to start in fullscreen mode

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,15 +1,18 @@
 package com.mygdx.game.desktop;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
+import java.awt.*;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Graphics.DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Drop";
-		config.width = 1920;
-		config.height = 1080;
+		config.setFromDisplayMode(displayMode);
+		config.title = "Angels and Demons";
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
